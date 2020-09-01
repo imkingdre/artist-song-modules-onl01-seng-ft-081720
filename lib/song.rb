8a687b6b79e79b48caa528c455d1fc3583d901ff
Andre King <andreking1031@gmail.com>
2020-09-01 22:16:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,9 @@ require 'pry'
 
 class Song
   extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
   
   attr_accessor :name
   attr_reader :artist
